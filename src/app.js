924ea340e0ce55a1719678c9d54d0ca35691cf17
Deborah Okeke <okekedeborah@gmail.com>
2023-08-15 17:04:17 +0100

    popup implementation complete

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 // reference html elements
+const body = document.querySelector('body')
 const main = document.querySelector('main')
 const nav = document.querySelector('nav')
 nav.className = 'nav'
+const popup = document.querySelector('.popup')
+const overlay = document.querySelector('.overlay')
+const closePopupBtn = document.createElement('span')
+
 
 // reference api url
 const movieApi = 'https://api.tvmaze.com/shows'
@@ -28,6 +33,14 @@ const displayMovies = async () => {
     // console.log(movieImage);
     const movieSummary = movieData.map(movie => movie.summary)
     // console.log(movieSummary);
+    const movieThumbnail = movieData.map(movie => movie.image.medium)
+    const movieRuntime = movieData.map(movie => movie.runtime)
+    const movieRating = movieData.map(movie => {
+        let rating = movie.rating
+        for (let key in rating) {
+            return `${key}: ${rating[key]}`
+        }
+    })
 
     let currentPage = 1
     let itemsPerPage = 8
@@ -73,35 +86,89 @@ const displayMovies = async () => {
             card.append(cardBody)
             main.append(card)
 
-            // create function to implement popup
-            function togglePopup () {
-                let popup = document.createElement('div')
+            // add event listener to show and hide popup
+            card.addEventListener('click', () => showPopup(i))
+            overlay.addEventListener('click', closePopup)
+            closePopupBtn.addEventListener('click', closePopup)
+            document.addEventListener('keydown', (event) => {
+                if (event.key === 'Escape' && !popup.classList.contains('hidden')) {
+                    closePopup()
+                }
+            })
 
-                let description = document.createElement('div')
-
-                let title = document.createElement('h3')
-                title.innerText = movieName[i]
-                console.log(title);
-
-                let summary = document.createElement('p')
-                summary.innerHTML = movieSummary[i]
-                console.log(summary);
-
-                description.append(title)
-                description.append(summary)
-
-                popup.append(description)
-                
-                main.append(popup)
-                popup.classList.toggle('show')
-            }
-            // togglePopup()
-
-            // add event listener on card to show popup
-            card.addEventListener('click', togglePopup)
         }
+
+        // create function to show popup
+        function showPopup (index) {
+            closePopupBtn.innerHTML = `<span class="material-symbols-outlined">
+            close
+            </span>`
+
+            const popupContainer = document.createElement('div')
+            popupContainer.className = 'popupContainer'
+
+            const description = document.createElement('div')
+            description.className = 'description'
+
+            const title = document.createElement('h3')
+            title.innerText = movieName[index]
+            console.log(title);
+
+            const summary = document.createElement('p')
+            summary.innerHTML = movieSummary[index]
+            console.log(summary);
+
+            description.append(title)
+            description.append(summary)
+
+            const extraDescription = document.createElement('div')
+            extraDescription.className = 'extraDescription'
+
+            const thumbnail = document.createElement('img')
+            thumbnail.src = movieThumbnail[index]
+
+            const runtime = document.createElement('span')
+            runtime.innerText = `RUNTIME: ${movieRuntime[index]}`
+
+            const rating = document.createElement('span')
+            rating.innerText = `RATING: ${movieRating[index]}`
+
+            const commentForm = document.createElement('form')
+            commentForm.className = 'commentForm'
+            const commentInput = document.createElement('input')
+            commentInput.placeholder = 'Your Name here'
+            const commentText = document.createElement('textarea')
+            commentText.placeholder = 'Your Comment here'
+            const commentButton = document.createElement('button')
+            commentButton.innerText = 'Comment'
+
+            commentForm.append(commentInput)
+            commentForm.append(commentText)
+            commentForm.append(commentButton)
+
+            extraDescription.append(thumbnail)
+            extraDescription.append(runtime)
+            extraDescription.append(rating)
+
+            popupContainer.append(extraDescription)
+            popupContainer.append(description)
+            popupContainer.append(commentForm)
+            
+            popup.append(closePopupBtn)
+            popup.append(popupContainer)
+
+            popup.classList.remove('hidden')
+            overlay.classList.remove('hidden')
+        }
+
+        // create function to close popup
+        function closePopup () {
+            popup.innerHTML = ''
+            popup.classList.add('hidden')
+            overlay.classList.add('hidden')
+        }
+
     }
-    
     
     // create page navigation
     const previousButton = document.createElement('span')
